7165e475ccc7158c5576aca4a4395f149e4cdcd9
Unknown <felipe.garcia.fernandez@gmail.com>
2018-03-24 09:32:34 +0100

    [felipe casa] video 54
    video 54

--- api/routes/message.js
+++ api/routes/message.js
@@ -5,4 +5,10 @@ var MessageController = require('../controllers/message');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
+api.post('/message',md_auth.ensureAuth,MessageController.saveMessage);
+api.get('/my-messages/:page?',md_auth.ensureAuth,MessageController.getReceivesMessages);
+api.get('/messages/:page?',md_auth.ensureAuth,MessageController.getEmitMessages);
+api.get('/unviewed-messages',md_auth.ensureAuth,MessageController.getUnViewedMessages);
+api.get('/set-viewed-messages',md_auth.ensureAuth,MessageController.setViewedMessages);
+
 module.exports = api;
