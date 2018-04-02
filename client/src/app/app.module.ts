6824c1d98e5a68c0d5576378501eca124d9a888f
fgarcia <felipe.garcia.fernandez@gmail.com>
2018-04-02 18:49:45 +0200

    [fgarcia] angular video 74

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
+// Cargar componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    routing,
+    HttpClientModule
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
